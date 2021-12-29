8e973069df8251aa227879348205b219e7fe4bc8
aivenkov <aleksandr.ivenkov@quadcode.com>
2021-12-29 22:17:02 +0300

    HW08 implement env reader

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bufio"
+	"errors"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+var ErrInvalidValue = errors.New("environment variables should not contain = symbols")
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +21,52 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	env := make(Environment, 10)
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, fileInfo := range files {
+		file, err := os.Open(dir + "/" + fileInfo.Name())
+		if err != nil {
+			return nil, err
+		}
+
+		if fileInfo.Size() == 0 {
+			env[fileInfo.Name()] = EnvValue{
+				NeedRemove: true,
+			}
+			continue
+		}
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			value, err := prepareValue(scanner.Text())
+			if err != nil {
+				return nil, err
+			}
+
+			envVal := EnvValue{
+				Value: value,
+			}
+			env[fileInfo.Name()] = envVal
+			break
+		}
+
+		if err := file.Close(); err != nil {
+			return nil, err
+		}
+	}
+
+	return env, nil
+}
+
+func prepareValue(value string) (string, error) {
+	value = strings.ReplaceAll(value, "\x00", "\n")
+	if strings.Contains(value, "=") {
+		return "", ErrInvalidValue
+	}
+
+	return strings.TrimRight(value, "\t "), nil
 }
