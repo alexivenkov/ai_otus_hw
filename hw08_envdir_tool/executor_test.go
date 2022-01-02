2dce8e4da8a4c4c9eb4414378b2acb5cb7c0a290
aivenkov <aleksandr.ivenkov@quadcode.com>
2022-01-02 22:07:48 +0300

    HW08 implement command executor

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,86 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	testCases := []struct {
+		testName  string
+		command   string
+		envValues Environment
+		osEnvs    map[string]string
+	}{
+		{
+			testName:  "without env",
+			command:   "echo",
+			envValues: nil,
+		},
+		{
+			testName: "single env",
+			command:  "echo",
+			envValues: Environment{
+				"BAR": EnvValue{
+					Value: "bar",
+				},
+			},
+		},
+		{
+			testName: "multiple env",
+			command:  "echo",
+			envValues: Environment{
+				"BAR": EnvValue{
+					Value: "bar",
+				},
+				"FOO": EnvValue{
+					Value: "foo",
+				},
+			},
+		},
+		{
+			testName: "unset env",
+			command:  "echo",
+			envValues: Environment{
+				"UNSET": EnvValue{
+					NeedRemove: true,
+				},
+			},
+			osEnvs: map[string]string{"UNSET": "UNSET"},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.testName, func(t *testing.T) {
+			for name, value := range tc.osEnvs {
+				err := os.Setenv(name, value)
+				require.NoError(t, err)
+			}
+
+			command := []string{tc.command}
+			exitCode := RunCmd(command, tc.envValues)
+			require.Equal(t, 0, exitCode)
+
+			for name, value := range tc.envValues {
+				require.Equal(t, os.Getenv(name), value.Value)
+			}
+		})
+	}
+}
+
+func TestRunCmdUnsuccessfulStatus(t *testing.T) {
+	f, err := os.Create("testdata/permitted")
+	defer os.Remove("testdata/permitted")
+	require.NoError(t, err)
+
+	err = f.Chmod(0000)
+	require.NoError(t, err)
+
+	cmd := []string{"cat", "testdata/permitted"}
+	code := RunCmd(cmd, Environment{})
+	require.Equal(t, 1, code)
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	args := os.Args
+
+	if len(args) < 3 {
+		fmt.Println("env path or executable command was not specify")
+		os.Exit(0)
+	}
+
+	path := args[1]
+	command := args[2:]
+
+	env, err := ReadDir(path)
+	if err != nil {
+		fmt.Println("unable to read env dir")
+		os.Exit(0)
+	}
+
+	exitCode := RunCmd(command, env)
+	os.Exit(exitCode)
 }
